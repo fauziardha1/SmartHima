cecd4f9a55e6d231700f5dcf17f8614ba052b9db
fauziarda <fauziardasaputra@gmail.com>
2019-09-09 20:38:26 +0700

    initial commit

---
+++ app/src/main/java/com/example/smarthima/Module/Tugas/TugasIndex.java
@@ -0,0 +1,16 @@
+package com.example.smarthima.Module.Tugas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.smarthima.R;
+
+public class TugasIndex extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tugas_index);
+    }
+}
---
+++ app/src/main/java/com/example/smarthima/Module/main.java
@@ -0,0 +1,4 @@
+package com.example.smarthima.Module;
+
+public class main {
+}
